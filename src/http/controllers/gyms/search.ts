fdb9acb6ba106f025df709a9a9ed7ba8fe8e079a
MauricioAires <mauricioaires015@gmail.com>
2023-12-04 14:21:41 -0300

    feat(gyms) routes and controllers

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeSearchGymUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsBodySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { page, q } = searchGymsBodySchema.parse(request.query)
+
+  const searchGymUseCase = makeSearchGymUseCase()
+
+  const { gyms } = await searchGymUseCase.execute({
+    query: q,
+    page,
+  })
+
+  return reply.status(200).send(gyms)
+}
